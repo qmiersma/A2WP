6ba0eb5899da9ea3b853e0f1490f933e8e16b3f7
Quinn Miersma <qmiersma@southbendin.gov>
2025-04-11 16:49:17 -0400

    Cleaned up code, created actUpdater, removed overhead

--- js/a2wp.js
+++ js/a2wp.js
@@ -1,5 +1,5 @@
 class A2WP {
-    constructor(getEndpoint, postEndpoint, templatePath, customFunc, targetPath) {
+    constructor({getEndpoint, postEndpoint, templatePath, customFunc, targetPath, args, timer}) {
         this.url1 = "https://amilia-proxy.azurewebsites.net/api/callamilia"; 
         this.url2 = "https://sbvpastg.wpenginepowered.com/wp-json/wp/v2/"; 
         this.getEndpoint = getEndpoint; 
@@ -7,12 +7,37 @@ class A2WP {
         this.templatePath = templatePath; 
         this.customFunc = customFunc; 
         this.targetPath = targetPath; 
+        this.args = args; 
+        this.timer = timer; 
     }
 
-    checkRun() {
+    checkRun(timer) {
+        let check = [false, false]; 
         const currentPath = window.location.pathname.split("/")[1]; 
 
-        return (currentPath == this.targetPath) ? true : false; 
+        check[0] = (currentPath == this.targetPath) ? true : false; 
+
+        if (localStorage.lastChecked == null) {
+            localStorage.setItem("lastChecked", new Date().getTime()); 
+            check[1] = true; 
+        } else {
+            const currentDate = new Date().getTime(); 
+
+            switch(timer) {
+                case "hour": 
+                    if (((currentDate - localStorage.lastChecked) / 3600000) >= 1) {
+                        check[1] = true; 
+                        localStorage.lastChecked = currentDate; 
+                    }
+                    break; 
+                case "day": 
+                    break; 
+                default: 
+                    check[1] = true; 
+            }
+        }
+    
+        return (check[0] && check[1]); 
     }
 
     async fetchTemplate(path) {
@@ -29,7 +54,7 @@ class A2WP {
         }
     }
 
-    async fetchData(url, method, site, endpoint = null) {
+    async fetchData({url, method, site, endpoint = null, arg = null}) {
         let fetchInfo = {
             method: method, 
             headers: {
@@ -38,6 +63,7 @@ class A2WP {
         }
 
         if (endpoint) fetchInfo.body = JSON.stringify({"endpoint": endpoint}); 
+        if (arg) url = `${url}?${arg}`; 
 
         try {
             const response = await fetch(url, fetchInfo).then(function(response) {
@@ -47,7 +73,7 @@ class A2WP {
     
                 return response.json(); 
             }).then(function(data) {
-                // console.log(data); <-- for debugging
+                console.log(data); // <-- for debugging
                 return data; 
             }); 
     
@@ -58,29 +84,33 @@ class A2WP {
     }
 
     // ADD TO POST: activity categories, age groups
-    async postData({url, endpoint, content, author = null, title = null, status = null, slug = null}) {
+    async postData({url, endpoint, content = null, author = null, title = null, status = null, slug = null, imgUrl = null}) {
+        let postInfo; 
+
+        postInfo = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json", 
+                "X-WP-Nonce": apiData.nonce
+            }, 
+            body: JSON.stringify({
+                "title": title, 
+                "status": status,
+                "slug": slug,
+                "content": content, 
+                "author": author
+            })
+        }
+
         try {
-            const response = await fetch(`${url}${endpoint}`, {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json", 
-                  "X-WP-Nonce": apiData.nonce
-                }, 
-                body: JSON.stringify({
-                    "title": title, 
-                    "status": status,
-                    "slug": slug,
-                    "content": content, 
-                    "author": author
-                })
-            }).then(function(response) {
+            const response = await fetch(`${url}${endpoint}`, postInfo).then(function(response) {
                 if (!response.ok) {
                     throw new Error("Unable to post data"); 
                 }
     
                 return response.json(); 
             }).then(function(data) {
-                // console.log(data); <-- for debugging
+                console.log(data); // <-- for debugging
                 return data; 
             }); 
     
@@ -90,8 +120,29 @@ class A2WP {
         }
     }
 
+    updateActDOM({page, amItem}) {
+        let locations = ""; 
+        amItem.Schedules[0].Locations.forEach(location => {
+            locations += `${location.Name}<br>`; 
+        });
+        const price = (amItem.Price != 0) ? `$${amItem.Price}` : "Free"; 
+    
+        page.querySelector(".amilia-wp-activity-schedule-summary").innerHTML = amItem.ScheduleSummary; 
+        page.querySelector(".amilia-wp-activity-dates").innerHTML = `${amItem.StartDate} to ${amItem.EndDate}`; 
+        page.querySelector(".amilia-wp-activity-location").innerHTML = locations; 
+        page.querySelector(".amilia-wp-activity-price").innerHTML = price; 
+        page.querySelector(".amilia-wp-activity-register-btn").href = amItem.SecretUrl; 
+        page.querySelector(".amilia-wp-activity-responsible-name").innerHTML = amItem.ResponsibleName; 
+        page.querySelector(".amilia-wp-activity-note").innerHTML = amItem.Note; 
+        page.querySelector(".amilia-wp-activity-img").src = amItem.PictureUrl; 
+        page.querySelector(".amilia-wp-activity-img").alt = `${amItem.Name} image`; 
+        page.querySelector(".amilia-wp-activity-descript").innerHTML = `<p>${amItem.Description}</p>`; 
+
+        return page; 
+    }
+
     async call() {
-        if (!this.checkRun()) return; 
+        if (!this.checkRun(this.timer)) return; 
 
         console.log("A2WP script is running"); 
         let template = await this.fetchTemplate(this.templatePath); 
@@ -99,87 +150,99 @@ class A2WP {
         if (!template) return; 
 
         let parser = new DOMParser, 
-            page = parser.parseFromString(template, "text/html"), 
-            wrapper1 = page.querySelector(".amilia-wp-page-wrapper-1"),
-            check;  
+            page = parser.parseFromString(template, "text/html"); 
 
-        this.fetchData(this.url1, "POST", "Amilia", "activities").then(amObj => {
-            if (typeof amObj == "undefined") return; 
+        const amObj = await this.fetchData({url: this.url1, method: "POST", site: "Amilia", endpoint: this.getEndpoint}); 
+        if (typeof amObj == "undefined") return; // No Amilia data to send
 
-            // *** Remove this when done testing; 
-            // currently only allows posting of 2 items
-            amObj = [amObj[0], amObj[1]]; 
-
-            amObj.forEach((amItem, amIndex, amArray) => {
-                this.fetchData(`${this.url2}${this.getEndpoint}`, "GET", "Wordpress").then(wpObj => {
-                    if (typeof wpObj == "undefined") return; 
-
-                    wpObj.some(function(wpItem) {
-                        check = (`activity-${amItem.Id}` == `${wpItem.slug}`); 
-                        // console.log(`activity-${amItem.Id} == ${wpItem.slug}`, check); <-- for debugging
-
-                        return check; // If true, breaks loop
-                    }); 
-
-                    if (!check) {
-                        // Everything that a custom function would possibly need 
-                        // should be passed in here
-                        this.customFunc({page: page, amItem: amItem, amIndex: amIndex, amArray: amArray, wrapper1: wrapper1, postData: this.postData, url: this.url2, endpoint: this.postEndpoint}); 
-                    }
-                }); 
-            }); 
-        }); 
+        const wpObj = await this.fetchData({url: `${this.url2}${this.getEndpoint}`, method: "GET", site: "Wordpress", arg: this.args.wp}); 
+        // Needs to do something if wpObj is null
+        
+        amObj.some(amItem => {
+            return this.customFunc({page: page, wpObj: wpObj, amItem: amItem, postData: this.postData, updateActDOM: this.updateActDOM, url: this.url2, endpoint: this.postEndpoint});
+        });
     }
 }
 
 // ---- Make your custom functions here ----
 
 // Modifies activity-template.html and pushes the new activity to WP
-function updateAct({page, amItem, postData, url, endpoint}) {
-    let locations = ""; 
-    amItem.Schedules[0].Locations.forEach(location => {
-        locations += `${location.Name}<br>`; 
-    });
-    const price = (amItem.Price != 0) ? `$${amItem.Price}` : "Free"; 
+function createAct({page, wpObj, amItem, postData, updateActDOM, url, endpoint}) {
+    let check; 
 
-    page.querySelector(".amilia-wp-activity-schedule-summary").innerHTML = amItem.ScheduleSummary; 
-    page.querySelector(".amilia-wp-activity-dates").innerHTML = `${amItem.StartDate} to ${amItem.EndDate}`; 
-    page.querySelector(".amilia-wp-activity-location").innerHTML = locations; 
-    page.querySelector(".amilia-wp-activity-price").innerHTML = price; 
-    page.querySelector(".amilia-wp-activity-register-btn").href = amItem.SecretUrl; 
-    page.querySelector(".amilia-wp-activity-responsible-name").innerHTML = amItem.ResponsibleName; 
-    page.querySelector(".amilia-wp-activity-note").innerHTML = amItem.Note; 
-    page.querySelector(".amilia-wp-activity-img").src = amItem.PictureUrl; 
-    page.querySelector(".amilia-wp-activity-img").alt = `${amItem.Name} image`; 
-    page.querySelector(".amilia-wp-activity-descript").innerHTML = `<p>${amItem.Description}</p>`; 
+    wpObj.some(function(wpItem) {
+        check = (`activity-${amItem.Id}` == `${wpItem.slug}`); 
+        console.log(`activity-${amItem.Id} == ${wpItem.slug}`, (`activity-${amItem.Id}` == `${wpItem.slug}`));
+
+        return check; 
+    }); 
+
+    if (!check) {
+        const newPage = updateActDOM({page: page, amItem: amItem}); 
     
-    // *** API TEST will be removed from title field once out of testing stage
-    postData({url: url, endpoint: endpoint, content: page.documentElement.innerHTML, author: 43, title: `API TEST: ${amItem.Name}`, status: "publish", slug: `activity-${amItem.Id}`}); 
+        // Creates featured media
+        // postData({url: url, endpoint: "media", imgUrl: amItem.PictureUrl})
+        //     .then(data => {
+        //         console.log("IMG ID -->", data); 
+        //     }); 
+        
+        // *** API TEST will be removed from title field once out of testing stage
+        postData({url: url, endpoint: endpoint, content: newPage.querySelector("body").innerHTML, author: 43, title: `API TEST: ${amItem.Name}`, status: "publish", slug: `activity-${amItem.Id}`}); 
+    
+        console.log(`Activity ${amItem.Id} created`);
+    }
 
-    console.log(`Activity ${amItem.Id} created`);
+    return false; 
 }
 
-// Modifies page-template.html and updates the things-to-do page
-function updatePage({page, amItem, amIndex, amArray, wrapper1, postData, url, endpoint}) {
-    let wrapper2 = page.createElement("div"); 
-    wrapper2.setAttribute("class", "amilia-wp-page-wrapper-2"); 
+function updateAct({page, wpObj, amItem, postData, updateActDOM, url, endpoint}) {
+    let check; 
 
-    wrapper2.innerHTML =    `<a class="amilia-wp-page-img-wrapper" href="https://sbvpastg.wpenginepowered.com/things-to-do/activity-${amItem.Id}">
-                                <img class="amilia-wp-page-img" src="${amItem.PictureUrl}" alt="${amItem.Name} image">
-                            </a>
-                            <h3 class="amilia-wp-page-name">${amItem.Name}</h3>
-                            <p class="amilia-wp-page-schedule">${amItem.ScheduleSummary}</p>`;
-    wrapper1.appendChild(wrapper2); 
+    wpObj.some(function(wpItem) {
+        check = (`activity-${amItem.Id}` == `${wpItem.slug}`); 
 
-    if (amIndex >= amArray.length - 1) {
-        postData({url: url, endpoint: endpoint, content: page.documentElement.innerHTML}); 
+        return check; 
+    }); 
+
+    if (check) {
+        const newPage = updateActDOM({page: page, amItem: amItem}); 
+    
+        postData({url: url, endpoint: endpoint, content: newPage.querySelector("body").innerHTML, status: "publish"}); 
+    
+        console.log(`Activity ${amItem.Id} updated`);
+
+        return true; 
     }
+
+    return false; 
 }
 
 // ---- Call your objects here ----
 
-let actUpdater = new A2WP("activities", "activities/", `${apiData.path}/html/activity-template.html`, updateAct, "things-to-do"); 
-actUpdater.call(); 
+let actCreator = new A2WP({
+    getEndpoint: "activities",
+    postEndpoint: "activities/", 
+    templatePath: `${apiData.path}/html/activity-template.html`, 
+    customFunc: createAct, 
+    targetPath: "things-to-do-2", 
+    args: {
+        "amilia": null, 
+        "wp": "per_page=100"
+    },
+    timer: "hour"
+});
+actCreator.call(); 
 
-let pageUpdater = new A2WP("activities", "pages/14323", `${apiData.path}/html/page-template.html`, updatePage, "things-to-do"); 
-pageUpdater.call(); 
+let actUpdater = new A2WP({
+    getEndpoint: "activities", 
+    postEndpoint: "activities/", 
+    templatePath: `${apiData.path}/html/activity-template.html`, 
+    customFunc: updateAct, 
+    targetPath: "things-to-do", 
+    args: {
+        "amilia": null, 
+        "wp": `slug=${window.location.pathname.split("/")[2]}`
+    },
+    timer: "none"
+});  
+actUpdater.call(); 
